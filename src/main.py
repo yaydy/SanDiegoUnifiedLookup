1dad8cc42910845ca3d4cc329cc2a49cdb977195
yaydy <107906477+yaydy@users.noreply.github.com>
2023-02-28 10:34:36 -0800

    fix

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import cvs
 print("╭━━━╮╱╱╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╭╮╱╭╮╱╱╱╱╭━╮╱╱╱╱╱╭┳╮╱╱╱╱╱╱╱╱╭╮\┃╭━╮┃╱╱╱╱╱╰╮╭╮┃╱╱╱╱╱╱╱╱╱┃┃╱┃┃╱╱╱╱┃╭╯╱╱╱╱╱┃┃┃╱╱╱╱╱╱╱╱┃┃\┃╰━━┳━━┳━╮╱┃┃┃┣┳━━┳━━┳━━┫┃╱┃┣━╮╭┳╯╰┳┳━━┳━╯┃┃╱╱╭━━┳━━┫┃╭┳╮╭┳━━╮\╰━━╮┃╭╮┃╭╮╮┃┃┃┣┫┃━┫╭╮┃╭╮┃┃╱┃┃╭╮╋╋╮╭╋┫┃━┫╭╮┃┃╱╭┫╭╮┃╭╮┃╰╯┫┃┃┃╭╮┃\┃╰━╯┃╭╮┃┃┃┣╯╰╯┃┃┃━┫╰╯┃╰╯┃╰━╯┃┃┃┃┃┃┃┃┃┃━┫╰╯┃╰━╯┃╰╯┃╰╯┃╭╮┫╰╯┃╰╯┃\╰━━━┻╯╰┻╯╰┻━━━┻┻━━┻━╮┣━━┻━━━┻╯╰┻╯╰╯╰┻━━┻━━┻━━━┻━━┻━━┻╯╰┻━━┫╭━╯\╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃\╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯")
 
-print("What year are you looking for?)
+print("What year are you looking for?")
 datayear = input('STATE')
 
 print("What name or job title are you looking for?")
 search_str = input('STATEMENT')
 
-with open('San Diego Data ' + datayear + '.csv', 'r') as csvfile:
+with open('San Diego Data {datayear}.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         for column in row:
